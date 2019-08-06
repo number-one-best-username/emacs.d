096516f9c8ee1169e4d0f30c1ff1a7825ff0984d
Thomas Paine <thomas@gnarly.dog>
2019-08-07 00:03:06 +1000

    Theme

--- custom.el
+++ custom.el
@@ -1,14 +1,28 @@
+;;; emacs --- custom config
+
+;;; Commentary:
+
+; Custom set
+
+;;; Code:
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes '(dracula))
-
- )
+ '(custom-safe-themes
+   '("b46ee2c193e350d07529fcd50948ca54ad3b38446dcbd9b28d0378792db5c088" default))
+ '(org-agenda-files '("~/Workspace/Todo.org"))
+ '(package-selected-packages
+   '(magit smartparens php-mode undo-tree web-mode flycheck dracula-theme counsel ivy swiper)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(provide 'custom)
+;;; custom.el ends here
--- init.el
+++ init.el
@@ -1,23 +1,49 @@
+;;; emacs --- config
 (package-initialize)
 
-;; Display
-(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
-(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
-(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+;;; Commentary:
 
+;; Emacs config
+
+;;; Code:
+
+;; Display
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
 (setq inhibit-startup-message t)
+
+;; Package archives
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/") t)
+
+;; Seperate custom file
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(load custom-file)
+
+(require 'smartparens-config)
+
+;; Enable global modes
+(ivy-mode 1)
+(counsel-mode 1)
+(global-flycheck-mode 1)
+(global-undo-tree-mode 1)
+(smartparens-global-mode 1)
+(global-undo-tree-mode 1)
+
+;; Hooks
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
 
 ;; Backups
 (setq backup-directory-alist
       `(("." . ,(expand-file-name
                  (concat user-emacs-directory "backups")))))
 
-;; Custom file
-(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
-(load custom-file)
-
-(ivy-mode 1)
-(counsel-mode 1)
-
 ;; Key Bindings
 (global-set-key (kbd "C-s") 'swiper)
+(global-set-key (kbd "C-c a") 'org-agenda)
+
+(desktop-save-mode 1)
+
+(provide 'init)
+;;; init.el ends here

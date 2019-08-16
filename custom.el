87571fdfb8248cc9d5feef9b194cfd5214fe7af8
Thomas Paine <thomas@gnarly.dog>
2019-08-16 16:23:54 +1000

    Add non git personal file

--- custom.el
+++ custom.el
@@ -13,10 +13,10 @@
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes '(dracula))
  '(custom-safe-themes
-   '("b46ee2c193e350d07529fcd50948ca54ad3b38446dcbd9b28d0378792db5c088" default))
+   '("274fa62b00d732d093fc3f120aca1b31a6bb484492f31081c1814a858e25c72e" "b46ee2c193e350d07529fcd50948ca54ad3b38446dcbd9b28d0378792db5c088" default))
  '(org-agenda-files '("~/Workspace/Todo.org"))
  '(package-selected-packages
-   '(magit smartparens php-mode undo-tree web-mode flycheck dracula-theme counsel ivy swiper)))
+   '(yaml-mode intero markdown-mode magit smartparens php-mode undo-tree flycheck dracula-theme counsel ivy swiper)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- init.el
+++ init.el
@@ -16,12 +16,24 @@
 ;; Package archives
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'load-path "/opt/mu/share/emacs/site-lisp/mu4e")
 
 ;; Seperate custom file
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
-(load custom-file)
-
 (require 'smartparens-config)
+(require 'mu4e)
+
+(load custom-file)
+(load (expand-file-name "personal.el" user-emacs-directory))
+
+;; mu4e
+(setq mail-user-agent 'mu4e-user-agent)
+(setq mu4e-mu-binary "/opt/mu/bin/mu")
+(setq mu4e-sent-folder   "/Sent")
+(setq mu4e-drafts-folder   "/Drafts")
+(setq mu4e-trash-folder   "/Trash")
+(setq mu4e-get-mail-command "mbsync -a")
+(setq send-mail-function 'smtpmail-send-it)
 
 ;; Enable global modes
 (ivy-mode 1)
@@ -29,7 +41,6 @@
 (global-flycheck-mode 1)
 (global-undo-tree-mode 1)
 (smartparens-global-mode 1)
-(global-undo-tree-mode 1)
 
 ;; Hooks
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
@@ -38,6 +49,8 @@
 (setq backup-directory-alist
       `(("." . ,(expand-file-name
                  (concat user-emacs-directory "backups")))))
+
+(defalias 'yes-or-no-p 'y-or-n-p)
 
 ;; Key Bindings
 (global-set-key (kbd "C-s") 'swiper)
